575ed15a94bb983abe18656ab5f340ddd5649bc1
linsizao <linsz@thinkive.com>
2023-11-21 14:28:06 +0800

    chore: first commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const a = (): string => {
+  console.log('aaa');
+  return 'aaa'
+}
+
+type Tb = string | number
+
+const b:Tb = 0
---
+++ src/utils/index.ts
@@ -0,0 +1,3 @@
+export function utils1(param1:string, param2:number):string {
+  return param1 + '-' + param1
+}
